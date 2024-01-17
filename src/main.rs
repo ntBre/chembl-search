18bb361710e1e8802a28614b158397ab61711a0a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-17 11:39:59 -0500

    add clap, take arguments from cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, HashSet};
 use std::sync::atomic::AtomicUsize;
 
+use clap::Parser;
 use openff_toolkit::ForceField;
 use rayon::iter::{ParallelBridge, ParallelIterator};
 use rsearch::find_matches;
@@ -15,19 +16,49 @@ fn load_want(path: &str) -> HashSet<String> {
         .collect()
 }
 
-fn main() {
-    let path = "/home/brent/omsf/chembl/chembl_33.sdf";
-    let m = SDMolSupplier::new(path);
+#[derive(Parser)]
+struct Cli {
+    /// The OpenFF force field to load from the toolkit.
+    #[arg(short, long, default_value = "openff-2.1.0.offxml")]
+    forcefield: String,
 
-    let forcefield = "openff-2.1.0.offxml";
-    let ff = ForceField::load(forcefield).unwrap();
-    let h = ff.get_parameter_handler("ProperTorsions").unwrap();
+    /// The `Parameter` type for which to extract parameters. Allowed options
+    /// are valid arguments to `ForceField.get_parameter_handler`, such as
+    /// Bonds, Angles, or ProperTorsions.
+    #[arg(short, long, default_value = "ProperTorsions")]
+    parameter_type: String,
+
+    /// The path to the SDF file from which to read Molecules.
+    #[arg(short, long, default_value = "chembl_33.sdf")]
+    molecule_file: String,
+
+    /// The path to the file listing the parameters to match against, one per
+    /// line.
+    #[arg(short, long, default_value = "want.params")]
+    search_params: String,
+
+    /// The number of threads to use. Defaults to the number of logical CPUs as
+    /// detected by rayon.
+    #[arg(short, long, default_value_t = 0)]
+    threads: usize,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let m = SDMolSupplier::new(cli.molecule_file);
+    let ff = ForceField::load(&cli.forcefield).unwrap();
+    let h = ff.get_parameter_handler(&cli.parameter_type).unwrap();
     let mut params = Vec::new();
     for p in h.parameters() {
         params.push((p.id(), ROMol::from_smarts(&p.smirks())));
     }
 
-    let want = load_want("want.params");
+    let want = load_want(&cli.search_params);
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(cli.threads)
+        .build_global()
+        .unwrap();
 
     let progress = AtomicUsize::new(0);
 
