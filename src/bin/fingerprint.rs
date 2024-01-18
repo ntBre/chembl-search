97e5422dce547bdae7d0ecfc6d2dd7ce5549c735
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-18 14:52:12 -0500

    use clap for fingerprint cli

--- src/bin/fingerprint.rs
+++ src/bin/fingerprint.rs
@@ -1,30 +1,53 @@
 use std::{fs::read_to_string, io::Write};
 
+use clap::Parser;
 use rsearch::{
     cluster::dbscan,
     matrix::Matrix,
     rdkit::{fingerprint::tanimoto, ROMol},
 };
 
-fn main() {
-    let mut fps = Vec::new();
-    // line counts for possible input smiles
-    // 17562 t123a.smiles
-    //    96 t138a.smiles
-    //  4245 t18a.smiles
-    //     8 t18b.smiles
-    //    14 t87a.smiles
-    // let inp = "t138a.smiles";
-    let inp = "t18a.smiles";
-    // let inp = "t87a.smiles";
-    for smiles in read_to_string(inp).unwrap().lines() {
-        let mut mol = ROMol::from_smiles(smiles);
-        mol.openff_clean();
-        let fp = mol.morgan_fingerprint_bit_vec::<1024>(4);
-        fps.push(fp);
-    }
+macro_rules! progress {
+    ($($t:tt)*) => {{
+        eprint!($($t)*);
+        std::io::stderr().flush().unwrap();
+    }};
+}
 
-    let start = std::time::Instant::now();
+// default parameters from 2020-03-05-OpenFF-Training-Data-Selection
+
+#[derive(Parser)]
+struct Cli {
+    /// The file of SMILES strings to read as input, one SMILES per line.
+    smiles_file: String,
+
+    /// DBSCAN parameter specifying the acceptable distance between a core point
+    /// of a cluster and one of its neighbors.
+    #[arg(short, long, default_value_t = 0.5)]
+    epsilon: f64,
+
+    /// DBSCAN parameter specifying the minimum number of points required to
+    /// form a dense region.
+    #[arg(short, long, default_value_t = 5)]
+    min_pts: usize,
+
+    /// Morgan fingerprinting radius
+    #[arg(short, long, default_value_t = 4)]
+    radius: u32,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let fps: Vec<_> = read_to_string(cli.smiles_file)
+        .unwrap()
+        .lines()
+        .map(|smiles| {
+            let mut mol = ROMol::from_smiles(smiles);
+            mol.openff_clean();
+            mol.morgan_fingerprint_bit_vec::<1024>(cli.radius)
+        })
+        .collect();
 
     let n = fps.len();
     let mut db = Matrix::zeros(n, n);
@@ -37,19 +60,12 @@ fn main() {
             db[(i, j)] = 1.0 - t;
             db[(j, i)] = 1.0 - t;
         }
-        print!("finished mol {i}\r");
-        std::io::stdout().flush().unwrap();
+        progress!("finished mol {i}\r");
     }
 
-    println!(
-        "finished tanimoto after {:.1} s",
-        start.elapsed().as_millis() as f64 / 1000.0
-    );
+    let labels = dbscan(&db, cli.epsilon, cli.min_pts);
 
-    // default parameters from 2020-03-05-OpenFF-Training-Data-Selection
-    // notebook are eps=0.5, min_pts=5
-    let labels = dbscan(&db, 0.5, 5);
-
+    // TODO some kind of useful output
     for group in labels.chunks(20) {
         for g in group {
             print!("{g:?}");
