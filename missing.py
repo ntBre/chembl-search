65dbcbe8089c868658d69d45b43f7cad1e3436ba
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-25 15:55:36 -0500

    ignore huge smiles outputs, add some utility stuff

---
+++ missing.py
@@ -0,0 +1,28 @@
+# compare want.params to files in output to see which parameters are not
+# covered by chembl
+
+from pathlib import Path
+
+
+def load_coverage(filename):
+    cover = {}
+    with open(filename) as inp:
+        for line in inp:
+            if line.startswith("t"):
+                sp = line.split()
+                cover[sp[0]] = int(sp[2])
+    return cover
+
+
+with open("want.params") as inp:
+    want = [line.strip() for line in inp]
+
+train_cover = load_coverage("../projects/valence-fitting/coverage/v2-tm.dat")
+test_cover = load_coverage(
+    "../projects/valence-fitting/coverage/v2-tm.industry.dat"
+)
+
+for w in want:
+    p = Path(f"output/{w}.smiles")
+    if not p.exists() and train_cover[w] == 0 and test_cover[w] == 0:
+        print(w)
