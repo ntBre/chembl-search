69bd23cb1a0967d8986aad6b29d2246fc28f6dfe
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-30 13:14:50 -0500

    update processing

--- process.sh
+++ process.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-params=( t112 t142j t116i t164 t8 t113 t7 t155 t126 t73h t89 t74h t142k t54 t63
-		 t55 t101 t133j t91 t100 t48a t141 t65 t30 t153 t142h t6 t12 t52 t56 t53
-		 t19a t88 t57 t18a t37 t128 t125 t124 t136 t116j t72h t115h t71h t158
-		 t122i t82a t134 t49 t123a t82h t135 t165 t99 t68 t141b t150 t81 t83h
-		 t141c t141a )
+params=( t141c t141a )
 
 default_pts=1
 default_eps=7
 
 for param in ${params[@]}; do
 	echo -e "\n$param"
+	firefox output/$param.html
 
 	brk=0
 	while [[ $brk != "y" ]]; do
@@ -24,13 +21,11 @@ for param in ${params[@]}; do
 		default_pts=$pts
 		default_eps=$eps
 
-		target/release/fingerprint \
+		time target/release/fingerprint \
 			output/$param.smiles \
 			--min-pts $pts \
 			--parameter $param \
 			--epsilon 0.$eps | wc -l
-
-		firefox output/$param.html
 
 		read -p "Accept? " brk
 	done
