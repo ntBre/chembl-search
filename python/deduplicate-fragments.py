52e4b616fdb09622328b9b1a976f288a1f0cfaf2
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-02 12:08:54 -0500

    add fragmenting files from lily

---
+++ python/deduplicate-fragments.py
@@ -0,0 +1,107 @@
+import logging
+import pathlib
+import typing
+from collections import defaultdict
+
+import click
+from click_option_group import optgroup
+
+from rdkit import Chem
+from rdkit.Chem import AllChem, Descriptors, Recap
+import tqdm
+
+
+def canonical_smiles(rd_molecule: Chem.Mol) -> str:
+    return Chem.MolToSmiles(
+        Chem.AddHs(rd_molecule), isomericSmiles=False, canonical=True
+    )
+
+
+
+@click.command()
+@click.option(
+    "--input",
+    "input_file",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.option(
+    "--reference",
+    "reference_files",
+    multiple=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
+)
+@click.option(
+    "--output",
+    "output_file",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False),
+    required=True,
+)
+def deduplicate(
+    input_file: str,
+    reference_files: typing.List[str],
+    output_file: str,
+):
+    from openff.toolkit.topology.molecule import Molecule
+    from openff.nagl.toolkits.openff import capture_toolkit_warnings
+
+    reference_smiles = defaultdict(set)
+    for reference_file in reference_files:
+        with open(reference_file, "r") as f:
+            all_smiles = [x.strip() for x in f.readlines()]
+        for smi in tqdm.tqdm(all_smiles, desc=f"Sorting {reference_file}"):
+            rdmol = Chem.MolFromSmiles(smi)
+            elements = []
+            for atom in rdmol.GetAtoms():
+                atom.SetAtomMapNum(0)
+                elements.append(atom.GetSymbol())
+            elements = tuple(sorted(elements))
+            reference_smiles[elements].add(canonical_smiles(rdmol))
+    
+    target_smiles = defaultdict(set)
+    with open(input_file, "r") as f:
+        smiles = [x.strip() for x in f.readlines()]
+
+    print(f"Loaded {len(smiles)} target SMILES")
+    for smi in tqdm.tqdm(smiles, desc="Sorting target"):
+        rdmol = Chem.MolFromSmiles(smi)
+        elements = []
+        for atom in rdmol.GetAtoms():
+            atom.SetAtomMapNum(0)
+            elements.append(atom.GetSymbol())
+        elements = tuple(sorted(elements))
+        canonical = canonical_smiles(rdmol)
+        if canonical in reference_smiles[elements]:
+            continue
+        else:
+            target_smiles[elements].add(canonical)
+
+
+    smiles_to_keep = []
+    with capture_toolkit_warnings():
+        for key, targets in tqdm.tqdm(target_smiles.items(), total=len(target_smiles)):
+            references = reference_smiles[key]
+            reference_molecules = [
+                Molecule.from_smiles(ref, allow_undefined_stereo=True)
+                for ref in references
+            ]
+            for target in tqdm.tqdm(targets, desc=f"{key}"):
+                offmol = Molecule.from_smiles(target, allow_undefined_stereo=True)
+                if any(
+                    offmol.is_isomorphic_with(other)
+                    for other in reference_molecules
+                ):
+                    continue
+                else:
+                    smiles_to_keep.append(target)
+
+    print(f"Keeping {len(smiles_to_keep)} SMILES")
+    with open(output_file, "w") as f:
+        f.write("\n".join(smiles_to_keep))
+
+
+
+        
+if __name__ == "__main__":
+    deduplicate()
---
+++ python/generate-fragments.py
@@ -0,0 +1,218 @@
+import logging
+import pathlib
+import typing
+from collections import defaultdict
+
+import click
+from click_option_group import optgroup
+
+from rdkit import Chem
+from rdkit.Chem import AllChem, Descriptors, Recap
+import tqdm
+
+
+def canonical_smiles(rd_molecule: Chem.Mol) -> str:
+    return Chem.MolToSmiles(
+        Chem.AddHs(rd_molecule), isomericSmiles=False, canonical=True
+    )
+
+
+def fragment_single(parent_smiles: str):
+    allowed_elements = {"Br", "C", "Cl", "F", "H", "I", "N", "O", "P", "S"}
+    rd_dummy_replacements = [
+        # Handle the special case of -S(=O)(=O)[*] -> -S(=O)(-[O-])
+        (Chem.MolFromSmiles("S(=O)(=O)*"), Chem.MolFromSmiles("S(=O)([O-])")),
+        # Handle the general case
+        (Chem.MolFromSmiles("*"), Chem.MolFromSmiles("[H]")),
+    ]
+    unique_fragments_by_n_heavy = defaultdict(set)
+
+    rd_parent: Chem.Mol = Chem.MolFromSmiles(parent_smiles)
+    if rd_parent is None:
+        return unique_fragments_by_n_heavy
+    
+
+    if any(
+        rd_atom.GetNumRadicalElectrons() != 0
+        or rd_atom.GetIsotope() != 0
+        or rd_atom.GetSymbol() not in allowed_elements
+        for rd_atom in rd_parent.GetAtoms()
+    ):
+        return unique_fragments_by_n_heavy
+
+    fragment_nodes = Recap.RecapDecompose(rd_parent).GetLeaves()
+    for fragment_node in fragment_nodes.values():
+        rd_fragment = fragment_node.mol
+
+        # clean fragment
+        for rd_dummy, rd_replacement in rd_dummy_replacements:
+            rd_fragment = AllChem.ReplaceSubstructs(
+                rd_fragment, rd_dummy, rd_replacement, True
+            )[0]
+            # Do a SMILES round-trip to avoid weird issues with radical formation...
+            rd_fragment = Chem.MolFromSmiles(Chem.MolToSmiles(rd_fragment))
+
+        if Descriptors.NumRadicalElectrons(rd_fragment) > 0:
+            logging.warning(f"A fragment of {parent_smiles} has a radical electron")
+            continue
+
+        fragment_smiles = canonical_smiles(rd_fragment)
+
+        if "." in fragment_smiles:
+            # Skip dimers, trimers, etc.
+            continue
+
+        n_heavy = rd_fragment.GetNumHeavyAtoms()
+        unique_fragments_by_n_heavy[n_heavy].add(fragment_smiles)
+    return unique_fragments_by_n_heavy
+
+def batch_fragment(smiles_list: list):
+    unique_fragments_by_n_heavy = defaultdict(set)
+    for parent_smiles in tqdm.tqdm(smiles_list):
+        parent_fragments_by_n_heavy = fragment_single(parent_smiles)
+        for n_heavy, fragment_smiles in parent_fragments_by_n_heavy.items():
+            unique_fragments_by_n_heavy[n_heavy] |= fragment_smiles
+    return unique_fragments_by_n_heavy
+
+@click.option(
+    "--input",
+    "input_paths",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False),
+    required=True,
+    multiple=True,
+)
+@click.option(
+    "--output",
+    "output_directory",
+    type=click.Path(exists=False, dir_okay=True, file_okay=False),
+    default="fragments",
+    help="The directory to the save the generated fragments to.",
+    show_default=True,
+)
+@optgroup.group("Parallelization configuration")
+@optgroup.option(
+    "--n-workers",
+    help="The number of workers to distribute the labelling across. Use -1 to request "
+    "one worker per batch.",
+    type=int,
+    default=1,
+    show_default=True,
+)
+@optgroup.option(
+    "--worker-type",
+    help="The type of worker to distribute the labelling across.",
+    type=click.Choice(["lsf", "local", "slurm"]),
+    default="local",
+    show_default=True,
+)
+@optgroup.option(
+    "--batch-size",
+    help="The number of molecules to processes at once on a particular worker.",
+    type=int,
+    default=500,
+    show_default=True,
+)
+@optgroup.group("LSF configuration", help="Options to configure LSF workers.")
+@optgroup.option(
+    "--memory",
+    help="The amount of memory (GB) to request per LSF queue worker.",
+    type=int,
+    default=3,
+    show_default=True,
+)
+@optgroup.option(
+    "--walltime",
+    help="The maximum wall-clock hours to request per LSF queue worker.",
+    type=int,
+    default=2,
+    show_default=True,
+)
+@optgroup.option(
+    "--queue",
+    help="The LSF queue to submit workers to.",
+    type=str,
+    default="cpuqueue",
+    show_default=True,
+)
+@optgroup.option(
+    "--conda-environment",
+    help="The conda environment that LSF workers should run using.",
+    type=str,
+)
+
+@click.command()
+def main(
+    input_paths,
+    output_directory,
+    worker_type: typing.Literal["lsf", "local"] = "local",
+    queue: str = "cpuqueue",
+    conda_environment: str = "openff-nagl",
+    memory: int = 4,  # GB
+    walltime: int = 32,  # hours
+    batch_size: int = 300,
+    n_workers: int = -1,
+):
+    from openff.nagl.utils._parallelization import batch_distributed
+    from dask import distributed
+
+    # Load in the molecules to fragment
+    all_parent_smiles = set()
+
+    for input_path in input_paths:
+        with open(input_path, "r") as file:
+            smiles = [x.strip() for x in file.readlines()]
+            smiles = [x for x in smiles if "." not in x]
+            all_parent_smiles |= set(smiles)
+
+    all_parent_smiles = sorted(all_parent_smiles, key=len, reverse=True)
+    print(f"Found {len(all_parent_smiles)} parent molecules")
+
+    unique_fragments_by_n_heavy = defaultdict(set)
+
+    with batch_distributed(
+        all_parent_smiles,
+        batch_size=batch_size,
+        worker_type=worker_type,
+        queue=queue,
+        conda_environment=conda_environment,
+        memory=memory,
+        walltime=walltime,
+        n_workers=n_workers,
+    ) as batcher:
+
+        futures = list(batcher(batch_fragment))
+        for future in tqdm.tqdm(
+            distributed.as_completed(futures, raise_errors=False),
+            total=len(futures),
+            desc="Saving"
+        ):
+            fragments_by_n_heavy = future.result()
+            for n_heavy, fragment_smiles in fragments_by_n_heavy.items():
+                unique_fragments_by_n_heavy[n_heavy] |= fragment_smiles
+
+    output_directory = pathlib.Path(output_directory)
+    output_directory.mkdir(parents=True, exist_ok=True)
+    full_path = output_directory / "fragments-full.smi"
+    small_path = output_directory / "fragments-small.smi"
+    # Save the fragments
+    with full_path.open("w") as file:
+        file.write(
+            "\n".join(
+                fragment_pattern
+                for n_heavy, fragment_smiles in unique_fragments_by_n_heavy.items()
+                for fragment_pattern in fragment_smiles
+            )
+        )
+
+    small_fragments = [
+        fragment_pattern
+        for n_heavy in range(3, 13)
+        for fragment_pattern in unique_fragments_by_n_heavy[n_heavy]
+    ]
+
+    with small_path.open("w") as file:
+        file.write("\n".join(small_fragments))
+
+
+if __name__ == "__main__":
+    main()
