d182a529117f50ff01ecf46443a0d9e57dac1a91
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-12 16:54:11 -0500

    add a test for long smiles that was very slow with openff_clean

--- src/rdkit/fragment.rs
+++ src/rdkit/fragment.rs
@@ -315,4 +315,22 @@ C(SSC(/C([H])=N/C([H])([H])C(=O)OC([H])([H])C([H])([H])[H])(C([H])([H])C([H])\
             assert!(got.contains_key(w));
         }
     }
+
+    #[test]
+    fn bad_recap() {
+        env_logger::init();
+        let smiles =
+            "[H]C1=C(C([H])([H])[C@@]([H])(C(=O)N([H])[C@@]2([H])C(=O)\
+N([H])[C@@]([H])(C([H])([H])c3c([H])nc([H])c([H])c3[H])C(=O)N([H])[C@]([H])\
+(C([H])([H])C3=C([H])N([H])c4c([H])c([H])c([H])c([H])c43)C(=O)N([H])[C@@]([H])\
+(C([H])([H])C([H])([H])C([H])([H])C([H])([H])N([H])[H])C(=O)N([H])[C@@]([H])\
+(C([H])([H])[C@]([H])(C([H])([H])[H])C([H])([H])S3=C([H])C([H])([H])C([H])([H])\
+C3([H])[H])C(=O)N([H])[C@]([H])(C(=O)N([H])[C@]([H])(C(=O)N([H])[H])C([H])([H])\
+c3c([H])c([H])c4c([H])c([H])c([H])c([H])c4c3[H])C([H])([H])SSC2([H])[H])N([H])\
+[H])c2c([H])c([H])c([H])c([H])c2N1[H]";
+        let mol = ROMol::from_smiles(smiles);
+        let fragments = recap_decompose(&mol, None);
+        let got = fragments.get_leaves();
+        assert_eq!(got.len(), 3);
+    }
 }
