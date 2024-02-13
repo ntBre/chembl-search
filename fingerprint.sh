2b578f67ced787150405ee462c24cf2200a1bda9
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-13 14:32:12 -0500

    work on fragmenting

--- fingerprint.sh
+++ fingerprint.sh
@@ -1,15 +1,24 @@
 export RUST_LOG=debug
 bin=target/release/fingerprint
 
-param=t61g
+#param=t138a
 eps=0.7
-pts=1
-fragment=
+pts=2
+fragment=-x
+
+# params=( t118h t142j t116i t89 t142k t101 t141 t88 t18a t116j t72h t115h t165
+# t141b t83h t141c )
+
+params=( t141 )
 
 set -x
 
-$bin output/$param.smiles -p $param -e $eps -m $pts $fragment -t 1
+for param in ${params[@]}; do
+	$bin output/$param.smiles -p $param -e $eps -m $pts $fragment -t 8
 
-if [[ $? ]]; then
-	firefox output/$param.html
-fi
+	if [[ $? -eq 0 ]]; then
+		firefox output/$param.html
+	else
+		echo error in $param
+	fi
+done
