3c7b8688f1a1cf5518d38d38637c3a6b917efe61
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-14 14:30:18 -0500

    use a longer smiles in the benchmark

--- benches/recap.rs
+++ benches/recap.rs
@@ -7,12 +7,16 @@ fn main() {
     divan::main();
 }
 
-// 2.111 ms initial mean
+// 5.9 ms initial mean
 
-#[divan::bench(args = [ROMol::from_smiles("[H]/C(=N/C([H])([H])C(=O)OC([H])([H])C([H])([H])[H])\
-C(SSC(/C([H])=N/C([H])([H])C(=O)OC([H])([H])C([H])([H])[H])(C([H])([H])C([H])\
-([H])[H])C([H])([H])C([H])([H])[H])(C([H])([H])C([H])([H])[H])C([H])([H])C([H])\
-([H])[H]")])]
+#[divan::bench(args = [ROMol::from_smiles("            [H]C1=C(C([H])([H])[C@@]([H])(C(=O)N([H])[C@@]2([H])C(=O)\
+N([H])[C@@]([H])(C([H])([H])c3c([H])nc([H])c([H])c3[H])C(=O)N([H])[C@]([H])\
+(C([H])([H])C3=C([H])N([H])c4c([H])c([H])c([H])c([H])c43)C(=O)N([H])[C@@]([H])\
+(C([H])([H])C([H])([H])C([H])([H])C([H])([H])N([H])[H])C(=O)N([H])[C@@]([H])\
+(C([H])([H])[C@]([H])(C([H])([H])[H])C([H])([H])S3=C([H])C([H])([H])C([H])([H])\
+C3([H])[H])C(=O)N([H])[C@]([H])(C(=O)N([H])[C@]([H])(C(=O)N([H])[H])C([H])([H])\
+c3c([H])c([H])c4c([H])c([H])c([H])c([H])c4c3[H])C([H])([H])SSC2([H])[H])N([H])\
+[H])c2c([H])c([H])c([H])c([H])c2N1[H]")])]
 fn recap(mol: &ROMol) -> RecapResult {
     recap_decompose(&mol, None)
 }
